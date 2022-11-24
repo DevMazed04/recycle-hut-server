900e098e5667cd8bdbacf65774751fbc017be44c
DevMazed04 <mdmazedislam2684@gmail.com>
2022-11-24 23:18:13 +0600

    use cors() and express.json() middlewares in the server

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+//middlewares
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
    res.send("Recycle Hut API is running");
