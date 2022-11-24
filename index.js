395cabdc4a43d0ff83867ce52269f54a86914f98
DevMazed04 <mdmazedislam2684@gmail.com>
2022-11-24 23:43:26 +0600

    use the environment variable by creating .env file

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 //middlewares
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vfwjfcr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
+
 
 app.get("/", (req, res) => {
    res.send("Recycle Hut API is running");
